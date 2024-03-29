c01f68615493e7046bb7c8fa877732e96bef5b56
Dmitriy Pertsev <davaeron@gmail.com>
2024-03-29 23:35:20 +0300

    0.1.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule DNSSRVCluster.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @scm_url "https://github.com/pertsevds/dns_srv_cluster"
   @maintainer "Dmitriy Pertsev"
 
